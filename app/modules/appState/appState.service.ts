5d201d7292a2fe73f7c378483d83105e3025f4cb
Kuba Jagoda <kuba.jagoda+github@gmail.com>
2015-11-10 08:11:52 +0100

    Added logout & loading

--- app/modules/appState/appState.service.ts
+++ app/modules/appState/appState.service.ts
@@ -38,7 +38,9 @@ export default class AppStateService {
             });
     }
 
-    logout(){}
+    logout() {
+        this.setToken('');
+    }
 
     getToken():string {
         return this.token;
--- app/modules/shoppingList/list/shoppingListList.component.ts
+++ app/modules/shoppingList/list/shoppingListList.component.ts
@@ -1,15 +1,22 @@
 import ShoppingListListApiService from "./shoppingListListApi.service";
+import AppStateService from "../../appState/appState.service";
+import IStateService = angular.ui.IStateService;
 export default function ShoppingListListComponent():angular.IDirective {
     return {
         restrict: 'E',
         scope: {},
         templateUrl: 'modules/shoppingList/list/shoppingListList.html',
-        controller(ShoppingListListApi:ShoppingListListApiService) {
+        controller($state: IStateService, ShoppingListListApi:ShoppingListListApiService, AppState:AppStateService) {
             this.loading = true;
             ShoppingListListApi.getShoppingLists().then(shoppingLists => {
                 this.shoppingLists = shoppingLists;
                 this.loading = false;
             });
+
+            this.logout = () => {
+                AppState.logout();
+                $state.go('user.login');
+            };
         },
         controllerAs: 'shoppingListListCtrl'
     }
